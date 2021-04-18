be621f54cc7cdc146098bc4e2b7782838cf7c833
YasinVeliyev <veliyev.yasin@gmail.com>
2021-04-18 10:57:32 +0400

    yeni dosya:           __pycache__/dbhelper.cpython-38.pyc 	değiştirildi:         crime.db 	yeni dosya:           crimemap.py 	yeni dosya:           dbhelper.py 	yeni dosya:           templates/home.html

---
+++ dbhelper.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+class DBHelper:
+    def connect(self, db='crime.db'):
+        return sqlite3.connect(db)
+    
+    def get_all_inputs(self):
+        with self.connect() as con:
+            cursor = con.cursor()
+            query = "SELECT description FROM crimes;"
+            cursor.execute(query)
+            return cursor.fetchall()
+    
+    def add_input(self, data):
+        with self.connect() as con:
+            query = "INSERT INTO crimes (description) VALUES(?)"
+            cursor = con.cursor() 
+            cursor.execute(query,(data,))
+            con.commit()
+            
+    
+    def clear_all(self):
+       with self.connect() as con:
+            cursor = con.cursor()
+            query = "DELETE FROM crimes"
+            cursor.execute(query)
+            con.commit()
+
